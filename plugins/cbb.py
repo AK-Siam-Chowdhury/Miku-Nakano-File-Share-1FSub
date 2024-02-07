68a8723c2dc56555b2801b08bebb3a7fa0b609fb
SiamRin33 <149977276+SiamRin33@users.noreply.github.com>
2024-02-07 14:31:38 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Siam19395/File-Sharing-Bot'>Click here</a>\n○ Channel : @Anime_Kun_Channel\n○ Support : @Chowdhury_Siam</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='@Chowdhury_Siam'>Click here</a>\n○ Channel : @Anime_Kun_Channel\n○ Support : @AnimeKunChannel</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
