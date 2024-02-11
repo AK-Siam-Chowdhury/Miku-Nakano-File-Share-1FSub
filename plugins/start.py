8969803d15ec6a4de63e4b2d20db4601cddd85f6
SiamRin33 <149977276+SiamRin33@users.noreply.github.com>
2024-02-12 03:40:30 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,9 +93,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        msg = await message.reply_text(
-              await asyncio.sleep(8)
-              await msg.delete()
+        await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
